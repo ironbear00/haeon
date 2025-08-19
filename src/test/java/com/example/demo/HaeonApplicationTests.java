8136899c734f7d4f3c3899736c7686be4cefd265
ironbear <20192492@g.dongseo.ac.kr>
2025-08-19 14:49:32 +0900

    수정: db 연결 test 코드 정리

--- src/test/java/com/example/demo/HaeonApplicationTests.java
+++ src/test/java/com/example/demo/HaeonApplicationTests.java
@@ -8,19 +8,8 @@ import javax.sql.DataSource;
 
 @SpringBootTest
 class HaeonApplicationTests {
-
-    @Autowired
-    private DataSource dataSource;
-
+    
 	@Test
 	void contextLoads() {
 	}
-
-    @Test
-    void testConnection() throws Exception {
-        try (var conn = dataSource.getConnection()) {
-            System.out.println("Connected to MariaDB: " + conn.getMetaData().getURL());
-        }
-    }
-
 }
