0484471f77dbb25b40df7adf7c164b5905dbcfa7
ironbear <20192492@g.dongseo.ac.kr>
2025-08-20 16:41:07 +0900

    추가: status, SnsPaltform 코드 테이블 생성 및 Init config로 값 넣기

---
+++ src/main/java/com/example/demo/domain/Status.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "status")
+public class Status {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String code; // SUBMITTED, PROCESSING, COMPLETED, REJECTED, DELETED, CANCELLED 등
+
+    @Column(length = 255)
+    private String description;
+}
