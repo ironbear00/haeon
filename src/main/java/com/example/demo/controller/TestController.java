a31846ccad71a7d192154936157d672e5b530396
ironbear <20192492@g.dongseo.ac.kr>
2025-08-25 14:46:25 +0900

    추가: main controller 수정: html 파일 refactor

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -16,7 +16,6 @@ public class TestController {
 
     private final UserService userService; // AuthService -> UserService로 변경
 
-    // 로그인된 사용자만 접근 가능한 핑 API
     @GetMapping("/ping")
     public ResponseEntity<UserResponse> ping(HttpSession session) {
         Object id = session.getAttribute(SESSION_USER_ID);
@@ -24,7 +23,6 @@ public class TestController {
             return ResponseEntity.status(401).build(); // 401 Unauthorized 반환
         }
         Long userId = (Long) id;
-        // 현재 로그인된 사용자 정보 반환
         return ResponseEntity.ok(userService.getProfile(userId));
     }
 }
