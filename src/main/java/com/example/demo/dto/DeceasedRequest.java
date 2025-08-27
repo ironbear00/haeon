b5ee213cca0edca0a3bbcbab363084204ef3af41
kandlin <kandlin@gmail.com>
2025-08-27 15:07:56 +0900

    추가 (마이페이지, 고인 html ) (고인 컨트롤러, 서비스, dto)

---
+++ src/main/java/com/example/demo/dto/DeceasedRequest.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+import com.example.demo.domain.utils.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class DeceasedRequest {
+    private String name;
+    private String phone;
+    private LocalDate birthDate;
+    private Gender gender;
+
+    private LocalDate deathDate;
+    private LocalDate funeralDate;
+
+}
