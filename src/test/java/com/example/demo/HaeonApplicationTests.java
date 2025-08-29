137987967359af952de9262410858443020fb839
LYS(Mac) <sella45@naver.com>
2025-08-29 16:46:59 +0900

    추가: 우측 하단 ai 챗봇 아이콘 추가

--- src/test/java/com/example/demo/HaeonApplicationTests.java
+++ src/test/java/com/example/demo/HaeonApplicationTests.java
@@ -28,7 +28,7 @@ import java.util.List;
 import java.util.Random;
 
 @SpringBootTest
-@ActiveProfiles("yamyam")
+@ActiveProfiles("seob")
 class HaeonApplicationTests {
 
     @Autowired private UserRepository userRepository;
